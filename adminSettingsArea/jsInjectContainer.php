5615698031875cfc21a42b59b59e0ceea3fb1132
Sean Dez <sd@seandezoysa.com>
2019-06-28 17:30:04 +0700

    reattach bundle file to php inject container

--- adminSettingsArea/jsInjectContainer.php
+++ adminSettingsArea/jsInjectContainer.php
@@ -1,6 +1,6 @@
 <div>
   <div id="adminRoot"></div>
-  <script src="<?php echo PLUGIN_FOLDER_URL . "adminSettingsArea/" ?>bundle.js"></script>
+  <script src="<?php echo PLUGIN_FOLDER_URL . "shared/" ?>adminArea.bundle.js"></script>
 </div>
 
 
