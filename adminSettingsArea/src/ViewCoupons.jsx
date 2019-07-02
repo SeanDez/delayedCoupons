e7b60c4110a5b9d933b648a2e48da18910b053ca
Sean Dez <sd@seandezoysa.com>
2019-07-03 05:20:45 +0700

    add dummy table

--- adminSettingsArea/src/ViewCoupons.jsx
+++ adminSettingsArea/src/ViewCoupons.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import {CurrentCouponChannel} from "./index.jsx";
 
 import {makeStyles} from "@material-ui/core/styles";
@@ -8,11 +8,34 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-
+import styled from "styled-components";
+import {FaChevronCircleLeft, FaChevronCircleRight} from 'react-icons/fa';
 
 
 export default props => {
-  const couponData = useContext(CurrentCouponChannel);
+  const couponData = useContext(CurrentCouponChannel),
+        styles = useStyles();
+  
+  // decides whether or not to render table
+  const checkifCouponDataExists = data => {
+    if (data && data.length) {
+      return true;
+    }
+    return false
+  };
+  
+  // renders body cell data
+  const renderTableBody = data => {
+    return data.map((record, i) => (
+      <TableRow key={i}>
+        <TableCell align='center'>{record.pageTarget}</TableCell>
+        <TableCell align='center'>{record.displayThreshold}</TableCell>
+        <TableCell align='center'>{record.numberOfOffers}</TableCell>
+        <TableCell align='center'>{Math.random() * 100}</TableCell>
+        <TableCell align='center'>delete</TableCell>
+      </TableRow>
+    ))
+  };
   
   ////// Side Effects //////
 
@@ -21,27 +44,38 @@ export default props => {
   
   return (
     <React.Fragment>
-      {console.log(couponData, `=====couponData=====`)}
       <h3>View Coupons</h3>
       <p>On this page you will find all the coupons you have setup and the pages they target. To delete a coupon click
          the delete icon to remove it.</p>
       
-      { true === true ?
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Target Page</TableCell>
-              <TableCell>Delay</TableCell>
-              <TableCell># of Offers</TableCell>
-              <TableCell>Total times coupon seen</TableCell>
-              <TableCell>Delete?</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
+      { checkifCouponDataExists(couponData) ?
+        <React.Fragment>
+          <StyledTable className={ styles.table }>
+            <TableHead>
+              <TableRow>
+                <TableCell align='center'>Target Page</TableCell>
+                <TableCell align='center'>Delay</TableCell>
+                <TableCell align='center'># of Offers</TableCell>
+                <TableCell align='center'>Total times coupon seen</TableCell>
+                <TableCell align='center'>Delete?</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              { renderTableBody(couponData) }
+            </TableBody>
+          </StyledTable>
         
-          </TableBody>
-        </Table>
-                      :
+          {/* Prev / Next Buttons*/}
+          <PrevNextContainer>
+            <FaChevronCircleLeft
+              className={styles.bigIcon}
+            />
+            <FaChevronCircleRight
+              className={styles.bigIcon}
+            />
+          </PrevNextContainer>
+        </React.Fragment>
+                                            :
         <p>No Coupons to Show</p>
       }
     </React.Fragment>
@@ -52,15 +86,32 @@ export default props => {
 
 const useStyles = makeStyles(theme => ({
   table : {
-  
+    border : '2px dashed red',
   },
-  
+  bigIcon : {
+    width : '30px',
+    height : 'auto',
+    color : 'blue',
+    border : '2px dashed green'
+  }
 }));
 
 
+////// Styled Components //////
 
+const StyledTable = styled(Table)`
+   max-width : 400px !important;
+   width : 400px !important;
+`;
 
-
+const PrevNextContainer = styled('div')`
+  margin-top : 20px;
+  display: flex;
+  flex-flow: row nowrap;
+  width: 80px;
+  justify-content: space-around;
+  border: 2px dashed pink;
+`;
 
 
 
