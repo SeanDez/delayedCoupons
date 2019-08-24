9c67bd3de609cd21b9e71b24a14aba2ed387b6e4
Sean Dez <sd@seandezoysa.com>
2019-08-24 21:08:03 +0700

    minor coupon tweak

--- frontFacingOverlay/coupon.php
+++ frontFacingOverlay/coupon.php
@@ -3,23 +3,42 @@
 <div id="outerContainer">
   <div id="headingContainer">
     <h1>
-      <?php echo $couponSettings->titleText ?>
+      <?php echo $couponSettings->titleText; ?>
     </h1>
   </div>
   
   <div id="descriptionContainer">
     <p>
-      <?php echo $couponSettings->descriptionText ?>
+      <?php echo $couponSettings->descriptionText; ?>
     </p>
   </div>
 </div>
 
 <style>
   #outerContainer {
-    height: 300px;
-    width: 600px;
+    padding: 20px 60px;
     display: flex;
     flex-flow: column nowrap;
-    justify-content: center;;
+    justify-content: space-around;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    border: 2px dashed <?php echo $couponSettings->descriptionTextColor; ?>
   }
+  
+  #headingContainer {
+    background-color: <?php echo $couponSettings->titleBackgroundColor; ?>
+  }
+  #headingContainer h1 {
+    color : <?php echo $couponSettings->titleTextColor; ?>
+  }
+  
+  #descriptionContainer {
+    background-color: <?php echo $couponSettings->descriptionBackgroundColor ?>;
+  }
+  #descriptionContainer h1 {
+    color : <?php echo $couponSettings->descriptionTextColor; ?>
+  }
+
+
 </style>
